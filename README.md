b7172f0d1cb4689e0a5db6ac5b1ea724624b2650
josmich100 <michinajosiah@gmail.com>
2023-11-03 01:27:22 +0000

    htrae

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # binary_trees
-tree data structure
--- binary_trees.h
+++ binary_trees.h
@@ -4,11 +4,9 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -16,12 +14,11 @@
  */
 struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
