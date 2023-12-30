28e906c9647b42f7c505cd63d5f00bc0864847a7
josmich100 <michinajosiah@gmail.com>
2023-12-30 19:47:23 +0000

    pushak

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,10 +6,10 @@
  * @tree: Pointer to the root node of the tree to be deleted.
  */
 void binary_tree_delete(binary_tree_t *tree){
-  if (tree == NULL)
-    return;
-  binary_tree_delete(tree->left);
-  binary_tree_delete(tree->right);
-  free(tree);
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
 
